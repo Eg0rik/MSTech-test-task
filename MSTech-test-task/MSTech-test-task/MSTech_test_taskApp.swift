b5507e67f7fe935810717b64fd833ba1c1604799
Egor <eg887or@gmail.com>
2026-01-02 12:57:08 +0300

    first commit

---
+++ MSTech-test-task/MSTech-test-task/MSTech_test_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSTech_test_taskApp.swift
+//  MSTech-test-task
+//
+//  Created by Egor on 2.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MSTech_test_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
